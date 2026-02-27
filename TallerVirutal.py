48532b06778f7c28b4bd7cfdafe1eb95f0caf488
Juan Pablo Moreno <juanpablomorenosirius@gmail.com>
2026-02-27 15:06:50 -0500

    2do

--- TallerVirutal.py
+++ TallerVirutal.py
@@ -2,7 +2,20 @@ class Persona:
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
+
     def presentarse(self):
         print("Hola, mi nombre es", self.nombre, "y tengo", self.edad, "años.")
-persona1 = Persona("Juan", 18)
-persona1.presentarse()       
+
+
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, carrera, semestre):
+        super().__init__(nombre, edad)
+        self.carrera = carrera
+        self.semestre = semestre
+
+    def estudiar(self):
+        print(self.nombre, "está estudiando", self.carrera)
+est1 = Estudiante("Juan", 18, "Ingeniería en Sistemas", 2)
+
+est1.presentarse()
+est1.estudiar()    
